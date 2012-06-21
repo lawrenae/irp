a65922835f9f177bcace9488d5213fd961e99906
Andy Lawrence <andy.lawrence@gmail.com>
2012-06-20 22:21:09 -0400

    initial rails 3.2.6 install with rspec and rspec-rails added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Irp::Application.config.secret_token = '47ac7fd23caed23751e07439b89d0569ae983702598305d4ca3f92eb22d3d247b8a2049370a712a0adb8918dfb41a554b1ca188e3979bf4647b7fe6dc1eb3819'
